653ffd05a07b40d0ce1636e7acbedd4e218a17e7
Andrii D <andrii.dashkovets@gmail.com>
2021-05-10 09:09:34 +0300

    update

--- scripts/ensemble/models.py
+++ scripts/ensemble/models.py
@@ -4,5 +4,10 @@ class Word:
         self.head = ""
         self.deprel = ""
         self.id = ""
+        self.lemma = ""
+        self.xpos = ""
+        self.feats = ""
+        self.misc = ""
+        self.text = ""
         self.uas_weight = 0
         self.las_weight = 0
